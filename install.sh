a05ba78080da5b7d78e181b9157e60fb894b835b
Raghavendra <77451747+cloudgrad@users.noreply.github.com>
2021-01-22 18:04:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install -y httpd php git
-sudo service httpd start
+sudo apt-get install -y apache2 php git
+sudo systemctl start apache2.service
 cd
 sudo git clone https://github.com/cloudgrad/az-alb-lab1.git
 sudo cp -r ./az-alb-lab1/* /var/www/html
