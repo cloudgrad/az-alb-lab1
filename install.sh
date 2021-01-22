4eae977730236b264de8dfa9944fdfc0f62f3d91
Raghavendra <77451747+cloudgrad@users.noreply.github.com>
2021-01-22 17:30:38 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum install -y httpd php git
+sudo service httpd start
+cd
+sudo git clone https://github.com/cloudgrad/az-alb-lab1.git
+sudo cp ./* /var/www/html
+sudo mv /var/www/html/htaccess /var/www/html/.htaccess
