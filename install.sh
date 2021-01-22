9d0c255d57455466c571374cf13f061fcfc1066a
Raghavendra <77451747+cloudgrad@users.noreply.github.com>
2021-01-22 17:58:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo yum install -y httpd php git
 sudo service httpd start
 cd
 sudo git clone https://github.com/cloudgrad/az-alb-lab1.git
-sudo cp -r ./* /var/www/html
+sudo cp -r ./az-alb-lab1/* /var/www/html
 sudo mv /var/www/html/htaccess /var/www/html/.htaccess
