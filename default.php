11288575ab26fe284426dfd34d16c9f96bb8d709
Raghavendra <77451747+cloudgrad@users.noreply.github.com>
2021-01-22 17:33:09 +0530

    Create default.php

---
+++ default.php
@@ -0,0 +1,13 @@
+<html>
+<title> CLOUDGRAD-AZURE-ALB-BASIC-LAB </title>
+<body>
+<h1>
+<center>
+<IMG SRC="123.jpg" ALT="123"><br>
+<?php
+echo $_SERVER['SERVER_ADDR'];
+?>
+</center>
+</h1>
+</body>
+</html>
